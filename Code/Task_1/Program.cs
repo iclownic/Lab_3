df6dac17860db80d2963fd9a472e01e94c80c4d2
Ludmila Pashkevych <161248085+iclownic@users.noreply.github.com>
2024-03-26 23:59:30 +0200

    Add files via upload

---
+++ Code/Task_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {       
+        static void Main(string[] args)
+        {
+            try
+            {
+                Set set1 = new Set();
+                set1.AddElement(12);
+                set1.AddElement(24);
+                set1.AddElement(-48);
+                set1.AddElement(8);
+
+                set1.JsonSerialize("C:\\Users\\iclownic\\Documents\\Visual Studio 2022\\Project\\IK_33_2term\\Lab_3\\set1.json");
+                Set set1Deserialized = set1.JsonDeserialize("C:\\\\Users\\\\iclownic\\\\Documents\\\\Visual Studio 2022\\\\Project\\\\IK_33_2term\\\\Lab_3\\\\set1.json");
+
+                set1Deserialized.RemoveElement(8);
+
+                Set set2 = new Set();
+                set2.AddElement(12);
+                set2.AddElement(24);
+                set2.AddElement(-48);
+                set2.AddElement(8);
+                
+                int x;
+                Console.Write("Число, яке ви хочете перевiрити на належнiсть: ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Чи належить {x} set1? {set1Deserialized.ContainsElement(x)} \nЧи належить {x} set2? {set2.ContainsElement(x)}");
+
+                Console.WriteLine($"Чи є set1 пiдмножиною set2? {set1Deserialized.SubsetSet(set2)}");
+
+                Console.WriteLine($"Чи рiвнi set1 та set2? {set1Deserialized.EqualSet(set2)}");
+
+                Set set3 = new Set();
+                set3.AddElement(12);
+                set3.AddElement(14);
+                set3.AddElement(-48);
+
+                Set set4 = new Set();
+                set4.AddElement(6);
+                set4.AddElement(14);
+                set4.AddElement(28);
+
+                Set union = set3.UnionElement(set4);
+                Console.Write("Об'єднання set3 та set4: ");
+                foreach (int element in union.elements)
+                    Console.Write(element + " ");
+
+                Set intersection = set3.IntersectionElement(set4);
+                Console.Write("\nПеретин set3 та set4: ");
+                foreach (int element in intersection.elements)
+                    Console.Write(element + " ");
+
+                Set difference = set3.DifferenceElement(set4);
+                Console.Write("\nРізниця set3 від set4: ");
+                foreach (int element in difference.elements)
+                    Console.Write(element + " ");
+
+                Set symmetricDiff = set3.SymmetricDifferenceElement(set4);
+                Console.Write("\nСиметрична різниця set3 та set4: ");
+                foreach (int element in symmetricDiff.elements)
+                    Console.Write(element + " " );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+            Console.ReadKey();
+        }       
+    }       
+}
---
+++ Code/Task_1/Set.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Task_1
+{
+    public class Set
+    {
+        public List<int> elements = new List<int>();
+
+        //належність
+        public bool ContainsElement(int element)
+        {
+            return elements.Contains(element);
+        }
+
+        //додавання
+        public void AddElement(int element)
+        {
+            if (!ContainsElement(element))
+                elements.Add(element);
+        }
+
+        //видалення
+        public void RemoveElement(int element)
+        {
+            elements.Remove(element);
+        }
+
+        //підмножина
+        public bool SubsetSet(Set set)
+        {
+            foreach (int element in elements)
+            {
+                if (!set.ContainsElement(element))
+                    return false;
+            }
+            return true;
+        }
+
+        //рівність (нерівність)
+        public bool EqualSet(Set set)
+        {
+            if (elements.Count != set.elements.Count)
+                return false;
+
+            foreach (int element in elements)
+            {
+                if (!set.ContainsElement(element))
+                    return false;
+            }
+            return true;
+        }
+
+        //об'єднання
+        public Set UnionElement(Set set)
+        {
+            Set union = new Set();
+            foreach (int element in elements)
+            {
+                union.AddElement(element);
+            }
+
+            foreach (int element in set.elements)
+            {
+                union.AddElement(element);
+            }
+            return union;
+        }
+
+        //перетин
+        public Set IntersectionElement(Set set)
+        {
+            Set intersection = new Set();
+            foreach(int element in elements)
+            { 
+                if (set.ContainsElement(element))
+                {
+                    intersection.AddElement(element);
+                }
+            }
+            return intersection;
+        }
+
+        //різниця
+        public Set DifferenceElement(Set set)
+        {
+            Set difference = new Set();
+            foreach (int element in elements)
+            {
+                if (!set.ContainsElement(element))
+                {
+                    difference.AddElement(element);
+                }
+            }
+            return difference;
+        }
+
+        //симетрична різниця
+        public Set SymmetricDifferenceElement(Set set)
+        {
+            Set symmetricDiff = new Set();
+            foreach (int element in elements)
+            {
+                if (!set.ContainsElement(element))
+                    symmetricDiff.AddElement(element);
+            }
+            foreach (int element in set.elements)
+            {
+                if (!ContainsElement(element))
+                    symmetricDiff.AddElement(element);
+            }
+            return symmetricDiff;
+        }
+
+        //серіалізація (JSON) та збереження у файл
+        public void JsonSerialize (string file)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(file, json);
+            Console.WriteLine($"Об'єкт збережено за посиланням: {file}");
+        }
+        //десеріалізація
+        public Set JsonDeserialize(string file)
+        {
+            string json = File.ReadAllText(file);
+            return JsonConvert.DeserializeObject<Set>(json);
+        }
+    }
+}
